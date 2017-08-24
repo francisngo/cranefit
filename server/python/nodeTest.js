b13a33e0e2774c725aa361c934295d90eb81494d
P-WM <P-WM@users.noreply.github.com>
2017-08-24 20:16:02 +0100

    WIP: nodejs send information to Prophet

--- server/python/nodeTest.js
+++ server/python/nodeTest.js
@@ -2,9 +2,18 @@
 const { spawn } = require('child_process');
 const py = spawn('python',  ['py_test.py']);
 
+// Get some dates
+const dates = new Array(7);
+const today = Date.now();
+let i = 7;
+while (i--) {
+  dates[i] = today - 86400000 * i;
+}
+
 // Use streams interface to send data to Pytohn process
 let dataString = '';
 py.stdout.on('data', data => dataString += data);
 py.stdout.on('end', () => console.log(dataString));
-py.stdin.write(JSON.stringify([1,2,3,4,5]));
+py.stderr.on('data', console.log);
+py.stdin.write(JSON.stringify([dates, [3,3,3,3,3,3,3]]));
 py.stdin.end();
--- server/python/py_test.py
+++ server/python/py_test.py
@@ -1,4 +1,7 @@
 ## py_test.py
+import pandas as pd
+import numpy as np
+from fbprophet import Prophet
 import sys, json
 
 #Read data from stdin and return first line
@@ -7,8 +10,11 @@ def read_in():
     return json.loads(lines[0])
 
 def main():
-    #Write array from stdin reversed to stdout
-    print(read_in()[::-1])
+    df = pd.DataFrame(columns=['ds','y'])
+    m = Prophet()
+    m.fit(df)
+    future = m.make_future_dataframe(periods=2)
+    print(future)
 
 #start process
 if __name__ == '__main__':
