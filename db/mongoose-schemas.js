c2c2ac659eb772148a7af1ba95b28f0c7dcbd235
Francis Ngo <francis.t.ngo@gmail.com>
2017-08-24 16:26:00 -0700

    Edit mongoose connection ready for crane-fit heroku and local

--- db/mongoose-schemas.js
+++ db/mongoose-schemas.js
@@ -1,21 +1,14 @@
+require('dotenv').config({ silent: true });
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.Promise = require('bluebird');
-require('dotenv').config({ silent: true });
-const mongoServer = process.env.MONGO_SERVER;
-const mongoUsername = process.env.MONGO_USERNAME;
-const mongoPassword = process.env.MONGO_PASSWORD
-const mongodbURI = `mongodb://${mongoUsername}:${mongoPassword}@${mongoServer}`
-
-mongoose.connect(mongodbURI, { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
 
 // Error Handling
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-
 //CREATING THE SCHEMA
-
 const userSchema = new Schema({
   partner_user_id: String,
   name: String,
