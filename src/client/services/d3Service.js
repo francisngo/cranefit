c87e94913c031ef196a64d62746cc8679ecec4d6
Francis Ngo <francis.t.ngo@gmail.com>
2017-08-25 16:18:22 -0700

    Hard code D3 chart into panel component (test)

--- src/client/services/d3Service.js
+++ src/client/services/d3Service.js
@@ -1,7 +1,6 @@
 //create factory which will enable us to inject the D3 module into custom directive
-angular.module('d3')
-  .factory('d3Service', [function() {
+angular.module('sparrowFit')
+  .factory('d3', [function() {
     var d3;
-    // insert d3 code here
     return d3;
   }]);
