bc5bc4d02202840b97b9f7b27beb4ed617b6a7ad
shaikathaque <shaikathaque4@gmail.com>
2017-08-25 12:25:02 -0700

    move createWorkout logic and ui

--- src/client/components/create-workout-component/createWorkout/createWorkout.js
+++ src/client/components/create-workout-component/createWorkout/createWorkout.js
@@ -1,6 +1,19 @@
 angular.module('sparrowFit')
-.controller('CreateWorkoutCtrl', function() {
+.controller('CreateWorkoutCtrl', function(httpService,  store) {
+  // object to store data to be sent to sever
+  this.data= {};
 
+  //addWorkout() create a template of named workout
+  this.addWorkout = function() {
+    this.data.name = this.name;
+    this.data.unitName = this.unitName;
+    this.data.unitValue = this.unitValue;
+
+    console.log('object to be sent to server: ', this.data);
+
+    // TODO: make POST request to server
+    // httpService.sendData('/api/workout', this.data);
+  };
 })
 .component('createWorkout', {
   controller: 'CreateWorkoutCtrl',
