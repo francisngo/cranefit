9d81b76f4331255c40b04e75658c7dc3692eb144
P-WM <P-WM@users.noreply.github.com>
2017-08-27 00:40:45 +0100

    Move db funcitonality form API to db module

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,26 @@
-module.exports = require('./schema');
+module.exports = exports = require('./schema');
+const { User } = exports;
+
+exports.retrieveUser = function retrieveUser(user_id) {
+  return User.find({ user_id }).exec()
+    .then(results => results[0]);
+};
+
+exports.retrieveUserSubDocs = function retrieveUserSubDocs(userId, subDocName) {
+  return exports.retrieveUser(userId)
+    .then(result => subDocName ? result[subDocName] : result)
+};
+
+exports.postUserSubDoc = function postUserSubDoc(userId, subDocName, body, res, isArray = true) {
+  let index = 0;
+    return exports.retrieveUser(userId)
+      .then((user) => {
+        if (isArray) {
+          index = user[subDocName].push(body) - 1;
+        } else {
+          user[subDocName] = body;
+        }
+        return user.save();
+      })
+      .then(user => isArray ? user[subDocName][index] : user[subDocName]); 
+};
