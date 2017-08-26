eafaec917b026f59623421059d39880a5ae9713d
Brian Kim <briankim4311@gmail.com>
2017-08-26 00:52:08 -0700

    Add editWorkout event handler

--- src/client/components/create-workout-component/createWorkout/createWorkout.js
+++ src/client/components/create-workout-component/createWorkout/createWorkout.js
@@ -1,13 +1,13 @@
 angular.module('sparrowFit')
-.controller('CreateWorkoutCtrl', function(httpService,  store) {
+.controller('CreateWorkoutCtrl', function(httpService, store) {
   // object to store data to be sent to sever
-  this.data= {};
+  this.data = {};
 
   //addWorkout() create a template of named workout
   this.addWorkout = function() {
     this.data.name = this.name;
-    this.data.unitName = this.unitName;
     this.data.unitValue = this.unitValue;
+    this.data.unitName = this.unitName;
 
     console.log('object to be sent to server: ', this.data);
 
--- src/client/components/workout-component/editWorkout/editWorkout.js
+++ src/client/components/workout-component/editWorkout/editWorkout.js
@@ -1,5 +1,21 @@
 angular.module('sparrowFit')
-.controller('editWorkoutCtrl', function() {
+.controller('editWorkoutCtrl', function(httpService) {
+
+  // To-Do: save workout selection to edit/update
+
+  // object to store data to be sent to server
+  this.data = {};
+
+  // new workout template with new values
+  this.editWorkout = function() {
+    this.data.name = this.newName;
+    this.data.unitValue = this.newUnitValue;
+    this.data.unitName = this.newUnitName;
+  };
+
+  console.log('object to be sent: ', this.data);
+
+  // httpService.updateData???('/api/workout', this.data);
 
 })
 .component('editWorkout', {
