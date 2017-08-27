8eb807d9f3035a25f20258f1e65154f0587059a7
shaikathaque <shaikathaque4@gmail.com>
2017-08-27 13:36:21 -0700

    start working on displaying progress for a goal

--- src/client/components/goal-component/panel/panel.js
+++ src/client/components/goal-component/panel/panel.js
@@ -1,9 +1,10 @@
 angular.module('sparrowFit')
-  .controller('panelCtrl', function() {
+  .controller('panelCtrl', function($scope, httpService) {
     // add workout data to scope
+    console.log($scope.selectedGoal)
     this.workoutData = [10,10,10];
   })
   .component('panel', {
-  controller: 'panelCtrl',
-  templateUrl: 'client/components/goal-component/panel/panel.html'
+    controller: 'panelCtrl',
+    templateUrl: 'client/components/goal-component/panel/panel.html'
 });
