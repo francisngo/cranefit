9f69c1aa3f1bc9fb8b0275d17d4788bfc473cc25
P-WM <P-WM@users.noreply.github.com>
2017-08-27 02:25:04 +0100

    Send goalNumber as number

--- src/client/components/goal-component/addGoal/addGoal.js
+++ src/client/components/goal-component/addGoal/addGoal.js
@@ -11,13 +11,16 @@ angular.module('sparrowFit')
 
   this.addGoal = function() {
     this.goal.workoutId = $scope.selectedWorkout._id;
-    this.goal.goalNumber = this.goalNumber;
+    this.goal.goalNumber = Number(this.goalNumber);
     this.goal.startDate = Date.now().valueOf();
     this.goal.endDate = this.goalEndDate.valueOf();
+<<<<<<< 491d329000da68027877720f0bd38a259d4c8b44
 
     alert('Goal Added!');
     console.log('object to be sent to server: ', this.goal);
 
+=======
+>>>>>>> Send goalNumber as number
     httpService.sendData('/api/goals', this.goal);
   };
 })
