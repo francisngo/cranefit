34339d11d3a02b3e02cf0f79d22014f14d19fd95
shaikathaque <shaikathaque4@gmail.com>
2017-08-27 12:04:46 -0700

    produce pie graph using data from controller

--- src/login/profile/profile-ctr.js
+++ src/login/profile/profile-ctr.js
@@ -1,9 +1,18 @@
 angular.module('sparrowFit')
   .controller('profileController', function(store, httpService) {
 
-    this.testMessage = 'Hello World';
-    this.workouts = {};
     this.profile = store.get('profile');
+    this.workoutData = [10,10,40];
+    this.testMessage = 'Hello World';
+    this.workouts = [
+          {name: "fake 1", unitValue: 8},
+          {name: "fake 2", unitValue: 12}
+        ];
+
+    this.exercises = [
+          {name: "Pull Ups", unitValue: 8},
+          {name: "Deadlifts", unitValue: 12}
+        ];
 
     httpService.getData('/api/workouts', function(workouts) {
       this.workouts = workouts;
