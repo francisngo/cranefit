040881e33d0c3ef3e84420e3d3853c9f4b5f10b1
Francis Ngo <francis.t.ngo@gmail.com>
2017-08-27 18:14:40 -0700

    Move "View Progress form" from goals.html to panel.html

--- src/client/components/goal-component/goals/goals.js
+++ src/client/components/goal-component/goals/goals.js
@@ -1,16 +1,16 @@
 angular.module('sparrowFit')
   .controller('GoalsCtrl', function($scope, httpService) {
-    $scope.goals = {};
-    
-    httpService.getData('/api/goals', function(goals) {
-      $scope.goals = goals;
-      $scope.selectedGoal = $scope.goals[0];
-      console.log($scope.goals);
-    });
-
-    this.handleClick = function() {
-      console.log('button clicked!');
-    }
+    // $scope.goals = {};
+    //
+    // httpService.getData('/api/goals', function(goals) {
+    //   $scope.goals = goals;
+    //   $scope.selectedGoal = $scope.goals[0];
+    //   console.log($scope.goals);
+    // });
+    //
+    // this.handleClick = function() {
+    //   console.log('button clicked!');
+    // }
   })
   .component('goals', {
     controller: 'GoalsCtrl',
