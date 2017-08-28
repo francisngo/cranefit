735e60b29f7a80077fa7ec84e21b3fe1a4d8d00e
shaikathaque <shaikathaque4@gmail.com>
2017-08-28 16:58:51 -0700

    remove unused service called

--- src/client/components/workout-component/workout.js
+++ src/client/components/workout-component/workout.js
@@ -1,50 +1,6 @@
 angular.module('sparrowFit')
-.controller('WorkoutCtrl', function WorkoutCtrl(httpService, userDataService, store) {
-  this.userData = {};
-  this.workoutData = '';
-  this.workout = '';
-  this.url = '/api/workouts/';
-
-  this.name = function(name) {
-    this.workoutData = userDataService.getWorkout(this.userData, name);
-    this.workout = this.workoutData.template;
-    this.timed = this.workoutData.timed;
-    if(this.timed===false){
-      this.untimed = true;
-    } else {
-      this.untimed = false;
-    }
-  };
-
-  httpService.getData(this.url, (returnValue) => {
-    this.userData = returnValue;
-
-    this.myTimedTemplates = this.userData
-    .filter((a) => {
-      return a.timed;
-    })
-    .map((a) => {
-      return a.templateName;
-    })
-    this.myUntimedTemplates = this.userData
-    .filter((b) => {
-      return b.timed === false;
-    })
-    .map((b) => {
-      return b.templateName;
-    })
-  });
-
-  this.getTimedTemps = function getTimedTemps() {
-    this.showTimed = this.myTimedTemplates;
-  };
-
-  this.getUntimedTemps = function getUntimedTemps() {
-    this.showUntimed = this.myUntimedTemplates;
-  };
-
+.controller('WorkoutCtrl', function WorkoutCtrl() {
 })
-
 .component('workout', {
   controller: 'WorkoutCtrl',
   templateUrl: 'client/components/workout-component/workout.html'
