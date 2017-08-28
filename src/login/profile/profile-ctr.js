88bdffa8d7d1cb59e302a8928468ba30bf272b8c
Brian Kim <briankim4311@gmail.com>
2017-08-28 01:24:00 -0700

    Change profile nickname to name and add join date of user from auth0

--- src/login/profile/profile-ctr.js
+++ src/login/profile/profile-ctr.js
@@ -2,6 +2,7 @@ angular.module('sparrowFit')
   .controller('profileController', function(store, httpService) {
 
     this.profile = store.get('profile');
+    this.joinedDate = this.profile.created_at.substring(0, 10);
     this.workoutData = [10,10,40];
     this.testMessage = 'Hello World';
     this.workouts = [
@@ -17,7 +18,7 @@ angular.module('sparrowFit')
     httpService.getData('/api/workouts', function(workouts) {
       this.workouts = workouts;
       console.log('profile controller called', this.workouts);
-    });  
+    });
   })
   .component('profile', {
     controller: 'profileController',
